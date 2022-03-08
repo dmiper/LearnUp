6de4facbb5da42bc1144984e6cd51f5da2e15fd3
dmiper <pervushin687@gmail.com>
2022-03-08 20:42:59 +0500

    Update

--- Task18/src/main/java/ru/learnup/Task18Application.java
+++ Task18/src/main/java/ru/learnup/Task18Application.java
@@ -19,42 +19,24 @@ public class Task18Application {
         Calculator calculator = applicationContext.getBean(Calculator.class);
 
         boolean gameOver = false;
-        while (gameOver == false) {
+        while (gameOver != true) {
             Arrays.asList("Выберите цифру операции: ", "1 - Суммировать", "2 - Вычесть ", "3 - Разделить ", "4 - Умножить ").forEach(System.out::println);
             int process = scanner.nextInt();
-            double arg1;
-            double arg2;
-            switch (process) {
-                case (1):
-                    System.out.println("Запишите значения для суммирования по порядку: ");
-                    arg1 = scanner.nextDouble();
-                    arg2 = scanner.nextDouble();
-                    System.out.println(arg1 + " - " + arg2 + " = " + calculator.sum(arg1, arg2));
-                    break;
-                case (2):
-                    System.out.println("Запишите значения для вычитания по порядку: ");
-                    arg1 = scanner.nextDouble();
-                    arg2 = scanner.nextDouble();
-                    System.out.println(arg1 + " - " + arg2 + " = " + calculator.subtract(arg1, arg2));
-                    break;
-                case (3):
-                    System.out.println("Запишите значения для деления по порядку: ");
-                    arg1 = scanner.nextDouble();
-                    arg2 = scanner.nextDouble();
-                    System.out.println(arg1 + " / " + arg2 + " = " + calculator.divide(arg1, arg2));
-                    break;
-                case (4):
-                    System.out.println("Запишите значения для умножения по порядку: ");
-                    arg1 = scanner.nextDouble();
-                    arg2 = scanner.nextDouble();
-                    System.out.println(arg1 + " * " + arg2 + " = " + calculator.multiply(arg1, arg2));
-                    break;
-                default:
-                    throw new RuntimeException("Не та цифра!!! Под такой цыфрой нет операции.");
-            }
+            if (process == 1) System.out.println("Запишите значения для суммирования по порядку: ");
+            else if (process == 2) System.out.println("Запишите значения для вычитания по порядку: ");
+            else if (process == 3) System.out.println("Запишите значения для деления по порядку: ");
+            else if (process == 4) System.out.println("Запишите значения для умножения по порядку: ");
+            else throw new RuntimeException("Не та цифра!!! Под такой цыфрой нет операции.");
+            double arg1 = scanner.nextDouble();
+            double arg2 = scanner.nextDouble();
+            if (process == 1) System.out.println(arg1 + " - " + arg2 + " = " + calculator.sum(arg1, arg2));
+            else if (process == 2) System.out.println(arg1 + " - " + arg2 + " = " + calculator.subtract(arg1, arg2));
+            else if (process == 3) System.out.println(arg1 + " / " + arg2 + " = " + calculator.divide(arg1, arg2));
+            else if (process == 4) System.out.println(arg1 + " * " + arg2 + " = " + calculator.multiply(arg1, arg2));
+            else throw new RuntimeException("Не та цифра!!! Под такой цыфрой нет операции.");
             Arrays.asList("Продолжить вычисления?", "1 - Yes.", "0 - No.").forEach(System.out::println);
             if (scanner.nextInt() == 0) gameOver = true;
         }
+        System.out.println("Конец программы");
     }
-
 }
