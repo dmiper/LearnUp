a6d0cd5466428e0d5e1d2fa1ccede678d056e56b
dmiper <pervushin687@gmail.com>
2022-03-25 21:59:03 +0500

    Update 1.0

---
+++ Task21/src/main/java/ru/learnup/task21/Task21Application.java
@@ -0,0 +1,15 @@
+package ru.learnup.task21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Task21Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Task21Application.class, args);
+
+        context.getBean(GuessTheNumber.class).print(context);
+    }
+}
